20450a80d042e8b473dfc01179a4caf073f0d74f
keshav pariyar <keshav.pariyar@elementure.tech>
2022-08-18 23:50:03 +0530

    aded python practice folder

---
+++ python/course.py
@@ -0,0 +1,20 @@
+# print("hello")
+# a=5
+# print(a)
+# data types in python 
+# int,float,boolean ,string complex
+  import turtle
+  
+def square():
+    kesh_turtle = turtle.turtle()
+    kesh_turtle.forward(100)
+    kesh_turtle.right(90)
+    kesh_turtle.forward(100)
+    kesh_turtle.right(90)
+    kesh_turtle.forward(100)
+    kesh_turtle.right(90)
+    kesh_turtle.forward(100)
+
+square()
+turtle.forward(300)
+square()
